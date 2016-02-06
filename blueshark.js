0a7b28b7057f01092b2f262e2c63ec8cfa6ea419
Sophie Stadler <srs2231@columbia.edu>
2016-02-05 22:39:33 -0500

    Add create form

--- blueshark.js
+++ blueshark.js
@@ -1,9 +1,34 @@
+Events = new Mongo.Collection("events");
+
 if (Meteor.isClient) {
 
   Template.hello.helpers({
   });
 
   Template.hello.events({
+    "submit .new-event": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var fbEventLink = event.target.link.value;
+      var recommendedPrice = event.target.price.value;
+      var cashName = event.target.cashtag.value;
+
+      // Events.insert({
+      //   recommendedPrice: recommendedPrice,
+      //   cashName: cashName
+      // })
+
+      var urlSplit = fbEventLink.split("/");
+      var eventId = urlSplit[4];
+      console.log(eventId);
+ 
+      // Clear form
+      // event.target.fb-event-link.value = "";
+      // event.target.recommended-price.value = "";
+      // event.target.cash-name.value = "";
+    }
   });
 
   // Routing
@@ -18,6 +43,29 @@ if (Meteor.isClient) {
       }
     });
   });
+
+  // FB pull
+  window.fbAsyncInit = function() {
+    console.log("Fb initi");
+    FB.init({
+      appId : '478980925626054',
+      status : true,
+      xfbml : true,
+      version : 'v2.4'
+    });
+    isFBinit = true;
+
+
+    FB.api(
+      "/{event-id}/attending",
+      function (response) {
+        if (response && !response.error) {
+          /* handle the result */
+          console.log("ok");
+        }
+      }
+  );
+  };
   
 }
 
