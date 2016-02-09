e470123beb7d267796d4f793df5bbcd28796667f
Ganesh Ravichandran <gr2483@columbia.edu>
2016-02-09 00:00:59 -0500

    working on socket.io

--- reVamped/public/js/index.js
+++ reVamped/public/js/index.js
@@ -1,9 +1,41 @@
+// instantiates socket
+var socket = io();
+
+// on Make Event button click
 $('.make-event').click( function(){
-  $('.make-event-form').show();
-  $('.find-event-form').hide();
+    $('.make-event-form').show();
+    $('.find-event-form').hide();
 });
 
+// on Find Event button click
 $('.find-event').click( function(){
-  $('.make-event-form').hide();
-  $('.find-event-form').show();
+    $('.make-event-form').hide();
+    $('.find-event-form').show();
+});
+
+// on Make Event submit
+$('#make-event-submit').click( function(){
+
+    // console.log each input in Make Event form
+    $('.make-event-form>input[type="text"]').each( function(index){
+        console.log($(this).val());
+    });
+});
+
+// on Make Event submit
+$('#find-event-submit').click( function(){
+
+    // console.log each input in Find Event form
+    $('.find-event-form>input[type="text"]').each( function(index){
+        console.log($(this).val());
+    });
+});
+
+$('input[name="link"]').click( function(){
+
+});
+
+socket.on('fbEventURL', function(msg){
+    console.log(msg);
+    $('.form-number').html(msg);
 });
